00b53521bac15b7e1373aa4ede5de644d54f81fc
Ashitosh Katale <149960334+AshKatale@users.noreply.github.com>
2025-03-17 10:18:43 +0530

    Update Liquidity_Monitoring_agent.py

--- Liquidity_Monitoring_agent.py
+++ Liquidity_Monitoring_agent.py
@@ -487,6 +487,6 @@ def get_token():
     
     return jsonify(report)
 
-if __name__ == '__main__':
-    # Running the Flask development server
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
